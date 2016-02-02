8f7340c2b839f0f7618bf0f7cf9027c4174c475e
Michael Tran <minhtran1995@hotmail.com>
2016-02-02 01:43:24 -0500

    Initial Commit

---
+++ Scripts/config/config.ts
@@ -0,0 +1,23 @@
+﻿module config {
+
+    // Scene Constants
+    export class Scene {
+        public static MENU: number = 0;
+        public static PLAY: number = 1;
+        public static OVER: number = 2;
+    }
+    
+    
+    // Screen Constants
+    export class Screen {
+        public static WIDTH: number = 1024;
+        public static HEIGHT: number = 600;
+        public static CENTER_X: number = 512;
+        public static CENTER_Y: number = 300;
+    }
+    
+    // Game Constants
+    export class Game {
+        public static FPS: number = 60;
+    }
+}
